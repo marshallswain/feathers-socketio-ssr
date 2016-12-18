d970cc234ec7634a2410a7f19acbec6785eb1acd
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2016-12-18 21:16:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ module.exports = function init (socket, isSsr, customRest) {
   if (!socket || !socket.io.uri) {
     throw new Error('You must pass an initialized socket to the feathers-socketio-ssr plugin.');
   }
-  const url = socket.io.uri;
   const isSSR = typeof isSsr === 'boolean' ? isSsr : isCommonSSR();
 
   return isSSR ? customRest : socketio(socket);
