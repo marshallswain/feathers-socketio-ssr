ab75dbbec160b423009d38e0c73af4b87d46b895
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2016-12-19 16:48:50 +0100

    Remove jQuery from peerDependencies (#3)
    * Update index.js
    
    * Update package.json
    
    * Update index.js
    
    * Update index.js
    
    * Update index.test.js
    
    * lint
    
    * Update README.md

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const socketio = require('feathers-socketio/client');
-const rest = require('feathers-rest/client');
-const jQuery = require('jquery');
 const isCommonSSR = require('./is-common-ssr');
 
 module.exports = function init (socket, isSsr, customRest) {
   if (!socket || !socket.io.uri) {
     throw new Error('You must pass an initialized socket to the feathers-socketio-ssr plugin.');
   }
-  const url = socket.io.uri;
   const isSSR = typeof isSsr === 'boolean' ? isSsr : isCommonSSR();
 
-  return isSSR ? customRest || rest(url).jquery(jQuery) : socketio(socket);
+  return isSSR ? customRest : socketio(socket);
 };
